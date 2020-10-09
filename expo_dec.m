2eeed90f89e10d590f35d8b7987326e3d64ce6df
cbshnrh <51054664+cbshnrh@users.noreply.github.com>
2020-10-09 14:23:56 +0800

    Update expo_dec.m

--- expo_dec.m
+++ expo_dec.m
@@ -1,3 +1,5 @@
+% Copyright (c) 2020 Key Laboratory for Information Science of Electromagnetic Waves (MoE),
+% Department of Communication Science and Engineering, Fudan University, Shanghai, China
 function [expo, num] = expo_dec(num, base)
 expo = 0;
 while mod(num, base) == 0
